53b94de4fbf2d7a4ba4b2339f480f86dae9d31cf
Oliver Thomsen <oliver@die-thomsens.de>
2024-06-04 09:21:03 +0200

    added version 2.2.224, updated version 2.0.206

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@
 BUILD_VERSION=1.4.200
 docker buildx build --push --build-arg H2_VERSION=$BUILD_VERSION --tag thomseno/h2:$BUILD_VERSION --platform=linux/amd64,linux/arm64 .
 
-# build latest version 2.0.2014
-BUILD_VERSION=2.0.2014
+# build latest version 2.0.206
+BUILD_VERSION=2.0.206
 docker buildx build --push --build-arg H2_VERSION=$BUILD_VERSION --tag thomseno/h2:$BUILD_VERSION --platform=linux/amd64,linux/arm64 .
 
-# build version 2.1.214 / latest
+# build version 2.1.214
 BUILD_VERSION=2.1.214
+docker buildx build --push --build-arg H2_VERSION=$BUILD_VERSION --tag thomseno/h2:$BUILD_VERSION --platform=linux/amd64,linux/arm64 .
+
+# build version 2.2.224 / latest
+BUILD_VERSION=2.2.224
 docker buildx build --push --build-arg H2_VERSION=$BUILD_VERSION --tag thomseno/h2:$BUILD_VERSION --tag thomseno/h2:latest --platform=linux/amd64,linux/arm64 .
